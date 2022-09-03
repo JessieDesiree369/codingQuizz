d5c3988efd15aa82e61ef646726fa536ea6d11df
rainey22598@gmail.com <rainey22598@gmail.com>
2022-09-02 20:36:07 -0400

    timer and question display, working on displaying questions

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
-## User Story
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-
-
 ## Acceptance Criteria
 ```
 GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
+
+~WHEN I click the start button
+~THEN a timer starts and I am presented with a question
+
 WHEN I answer a question
 THEN I am presented with another question
 WHEN I answer a question incorrectly
--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,73 @@
 let initials = document.getElementById("usrInitials")
-var qDisplay = document.getElementById("qDisplay")  
+var qDisplay = document.getElementById("qDisplay")
 var optionsDiv = document.getElementById("optionsDiv")
+var showTime = document.getElementById("timeDisplay")
 
-  var theQuestions = [
-    {
-      question: "Commonly used data types DO NOT include:",
-      options: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      question: "The condition in an if / else statement is enclosed within ____.",
-      options: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-      question: "Arrays in JavaScript can be used to store ____.",
-      options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      answer: "all of the above"
-    },
-    {
-      question: "String values must be enclosed within ____ when being assigned to variables.",
-      options: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      question:"A very useful tool used during development and debugging for printing content to the debugger is:",
-      options: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    }
-  ];
+var optn1 = document.getElementById("abtn1")
+var optn1 = document.getElementById("abtn2")
+var optn1 = document.getElementById("abtn3")
+var optn1 = document.getElementById("abtn4")
+
+var questionIndex = 0;
+
+var theQuestions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    question: "The condition in an if / else statement is enclosed within ____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
 
 
 function runQuiz() {
-    document.getElementById("startScreen").style.display = "none";
-    document.getElementById("theGame").style.display = "block"
-    displayQandAs()
-}
-    //loop for each question
-var displayQandAs = function() {
-    for (var i = 0; i < theQuestions.length; i++) {
-    qDisplay.innerText = theQuestions[i].question
+  // hide start screen & show q/a screen
+  document.getElementById("startScreen").style.display = "none";
+  document.getElementById("theGame").style.display = "block"
 
-      // loop to create and append buttons for each option
-        // for (var x = 0; x < theQuestions[i].options.length; x++) {
-        // var answerbutton = document.createElement('button')
-        // answerbutton.innerText = options[x]
-        // optionsDiv.append(answerbutton)
-        // }
+  //countdown begins
+  var timelft = 60;
+  var counter = setInterval(countdown, 1000);
+
+  function countdown() {
+    timelft = timelft - 1;
+    if (timelft <= 0) {
+      clearInterval(counter);
+      //countdown ended, display initials input screen
+      return;
     }
+    // code for showing the number of seconds left <here>
+    showTime.innerText = timelft
+  }
+  displayQandAs()
+}
+
+var displayQandAs = function () {
+  // ask question
+  var askingRN = theQuestions[questionIndex];
+  qDisplay.innerText = askingRN.question
+  // show corresponding questions !!!NOT WORKING YET!!!
+  optn1.textContent = askingRN[options][0];
+  console.log(optn1)
 };
+
+
